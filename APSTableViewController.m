2e482c59d1236a9be643417bba9f6a6fd321d842
Andy Sharkey <apsharkey@gmail.com>
2014-09-14 10:34:42 -0700

    Added Sections, Labeling, Colors

--- APSTableViewController.m
+++ APSTableViewController.m
@@ -26,46 +26,64 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    if (section == 0)
+    {
+        return 2;
+    }
+    else if (section == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 3;
+    }
+    
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
+    static NSString *cellIdentifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+
     // Configure the cell...
+    if (indexPath.section == 0)
+    {
+        cell.textLabel.text = @"I am in section 0";
+        cell.backgroundColor = [UIColor redColor];
+    }
+    if (indexPath.section == 1)
+    {
+        cell.textLabel.text = @"another section";
+        cell.backgroundColor = [UIColor blueColor];
+    }
+    if (indexPath.section == 2)
+    {
+        cell.textLabel.text = [NSString stringWithFormat:@"cell %i", indexPath.row];
+        cell.backgroundColor = [UIColor yellowColor];
+    }
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
